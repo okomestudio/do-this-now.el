35b0ba1b592efaa0ac936b0a0a6cb71a53538bd3
Taro Sato <okomestudio@gmail.com>
2024-09-16 23:36:46 -0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # do-this-now.el
+
+A notification scheduler for Emacs.
+
+It is very easy to spend too much time in front of Emacs. A regular reminder for doing
+something other than sitting in front of a computer would be nice. Maybe you should stand
+upand do some stretch every hour or so. Or may be make a cup of coffee.
+
+This tool sets up such a reminder.
+
+The `do-this-now.el` uses [alert.el](https://github.com/jwiegley/alert) as an interface to
+access the OS's notification facility.
+
+## Usage
+
+With `use-package`, have the following in the `init-el` file:
+
+``` emacs-lisp
+(use-package do-this-now
+  :straight (do-this-now
+             :host github
+             :repo "okomestudio/do-this-now.el")
+  :custom ((alert-default-style 'notifications)
+           (do-this-now-interval 2400)                 ; notify in 40 minutes
+           (do-this-now-message "Hey, do this now!!")
+           (do-this-now-title "Do This Now!"))
+  :demand t)
+```
+
+The notification itself is managed entirely by `alert.el`; see its documentation for finer
+control over how the message is displayed.
+
+## TODOs
+
+- [ ] Look for a solution to display notification more prominently in GUI
--- do-this-now.el
+++ do-this-now.el
@@ -1,1 +1,75 @@
+;;; do-this-now.el --- Do This Now  -*- lexical-binding: t -*-
+;;
+;; Copyright (C) 2024 Taro Sato
+;;
+;; Author: Taro Sato <okomestudio@gmail.com>
+;; URL: https://github.com/okomestudio/
+;; Version: 0.1
+;; Keywords:
+;; Package-Requires: ((emacs "29.1") (alert "1.2"))
+;;
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; A notification scheduler for Emacs.
+;;
+;;; Code:
+
+(require 'alert)
+
+(defgroup do-this-now nil
+  "Group for `do-this-now'."
+  :prefix "do-this-now-")
+
+(defcustom do-this-now-interval 2400
+  "Time in second before alert trigger."
+  :group 'do-this-now)
+
+(defcustom do-this-now-message "Hey, do this now!!"
+  "Message to show."
+  :group 'do-this-now)
+
+(defcustom do-this-now-title "Do This Now!"
+  "Message title to show."
+  :group 'do-this-now)
+
+(defvar do-this-now--timer nil
+  "Timer object for alert. Use `cancel-timer' to cancel a scheduled alert.")
+
+(defun do-this-now-alert ()
+  (alert do-this-now-message
+         :title do-this-now-title
+         :severity 'high)
+  (do-this-now-setup-next-alert))
+
+(defun do-this-now-start-timer-for-next-alert ()
+  (when do-this-now--timer
+    (cancel-timer do-this-now--timer))
+  (setq do-this-now--timer
+        (run-with-timer do-this-now-interval nil #'do-this-now-alert)))
+
+(defun do-this-now--hook ()
+  (do-this-now-start-timer-for-next-alert)
+  (remove-hook 'post-command-hook #'do-this-now--hook))
+
+(defun do-this-now-setup-next-alert ()
+  (add-hook 'post-command-hook #'do-this-now--hook))
+
+(do-this-now-setup-next-alert)
+
 (provide 'do-this-now)
+;;; do-this-now.el ends here
